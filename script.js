e7d5ee64e251d8deb447de88d61a1c5655b2642e
parulloyal <139440794+parulloyal@users.noreply.github.com>
2023-07-18 11:24:12 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function changeText() {
-    let textsArray = ['Text 1','Text 2','Text 3','Text 4'];
-    let number = getRandomNumber(0, textsArray.length - 1);
-    console.log(number);
-    document.getElementById('heading').innerHTML = textsArray[number];
+let potentialTexts = ['Text 1','Text 2','Text 3','Text 4','Text5'];
+let number = getRandomNumber(0, potentialTexts.length - 1);
+console.log(number);
+let textToChangeTo = potentialTexts[number];
+document.getElementById('heading').innerHTML = textToChangeTo;
 }
 
 function getRandomNumber(min, max) {

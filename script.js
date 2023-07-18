90f143f2284091c380b3d9ad560f776e479567e3
parulloyal <139440794+parulloyal@users.noreply.github.com>
2023-07-18 10:47:34 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function changeText() {
     let textsArray = ['Text 1','Text 2','Text 3','Text 4'];
     let number = getRandomNumber(0, textsArray.length - 1);
+    console.log(number);
     document.getElementById('heading').innerHTML = textsArray[number];
 }
 
